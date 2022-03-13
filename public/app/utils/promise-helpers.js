aa48353cb52f85360e690f5277140f69842a5be1
Yuri <yurialves2@gmail.com>
2022-03-12 21:09:33 -0300

    chore: add log helper for promises

--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -2,3 +2,8 @@ export const handleStatus = (response) => {
   if (response.ok) return response.json();
   return Promise.reject(response.statusText);
 };
+
+export const log = (param) => {
+  console.log(param);
+  return param;
+};
