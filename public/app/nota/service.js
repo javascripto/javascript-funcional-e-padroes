627c87c54813946e8b57a64728fec242a67b2345
Yuri <yurialves2@gmail.com>
2022-03-12 23:38:26 -0300

    chore: create partialize, compose, pipe functions

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,12 +1,18 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize, pipe } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
-const sumItems = (code) => (notas) =>
-  notas
-    .$flatMap((nota) => nota.itens)
-    .filter((item) => item.codigo === code)
-    .reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotes = (notes) => notes.$flatMap((nota) => nota.itens);
+
+const filterItemsByCode = (code, items) =>
+  items.filter((item) => item.codigo === code);
+
+const sumItemsValues = (items) =>
+  items.reduce((total, item) => total + item.valor, 0);
+
+const sumItems = (code) =>
+  pipe(getItemsFromNotes, partialize(filterItemsByCode, code), sumItemsValues);
 
 export const notasService = {
   listAll() {
---
+++ public/app/utils/operators.js
@@ -0,0 +1,28 @@
+// // Aplicação parcial
+// const ehDivisivel = (divisor, numero) = !(numero % divisor); // função de 2 argumentos
+// const ehDivisivelPorDois = ehDivisivel.bind(null, 2); // convertendo para função de 1 argumento pois o primeiro já foi passado
+// const ehDivisivelPorDois = (numero) => ehDivisivel(2, numero); // Mesmo que o código de cima
+
+export const partialize = (fn, ...args) => {
+  return fn.bind(null, ...args);
+};
+
+// Código de cima de outra forma
+const partialize2 = (fn, ...fnOriginalArgs) => {
+  return (...restArgs) => fn(...fnOriginalArgs, ...restArgs);
+};
+
+// point free functions
+// funções que em momento nenhum fazem referencia a parâmetros
+// ex: compose(toUpperCase, trim) // toUpperCase(trim(string))  // retorna uma função para trabalhar com string mas em momento nenhum isso foi informado
+export function compose(...fns) {
+  return (firstArgument) =>
+    fns.reduceRight((composed, fn) => fn(composed), firstArgument);
+}
+
+// Pipe é como o compose porém a order das funções é a ordem de execução tornando o código mais legível
+// ex: pipe(trim, upperCase) // s.trim().toUpperCase()
+export function pipe(...fns) {
+  return (firstArgument) =>
+    fns.reduce((composed, fn) => fn(composed), firstArgument);
+}
