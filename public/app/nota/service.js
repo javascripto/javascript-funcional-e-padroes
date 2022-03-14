18fc1871f422f94ede50bb69ea731dfec5966195
Yuri <yurialves2@gmail.com>
2022-03-14 07:39:27 -0300

    chore: using maybe on notes service

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,24 +1,31 @@
+// @ts-check
+import { Maybe } from '../utils/maybe.js';
 import { handleStatus } from '../utils/promise-helpers.js';
 import { partialize, pipe } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromNotes = (notes) => notes.$flatMap((nota) => nota.itens);
+const getItemsFromNotes = (notesM) =>
+  notesM.map((notes) => notes.$flatMap((nota) => nota.itens));
 
-const filterItemsByCode = (code, items) =>
-  items.filter((item) => item.codigo === code);
+const filterItemsByCode = (code, itemsM) =>
+  itemsM.map((items) => items.filter((item) => item.codigo === code));
 
-const sumItemsValues = (items) =>
-  items.reduce((total, item) => total + item.valor, 0);
+const sumItemsValues = (itemsM) =>
+  itemsM.map((items) => items.reduce((total, item) => total + item.valor, 0));
 
 const sumItems = (code) =>
   pipe(getItemsFromNotes, partialize(filterItemsByCode, code), sumItemsValues);
 
 export const notasService = {
   listAll() {
-    return fetch(API).then(handleStatus);
+    return fetch(API)
+      .then(handleStatus)
+      .then((notas) => Maybe.of(notas));
   },
   sumItems(code) {
-    return this.listAll().then(sumItems(code));
+    return this.listAll()
+      .then(sumItems(code))
+      .then((totalM) => totalM.getOrElse(0));
   },
 };
